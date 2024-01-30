385bf895b139de546825bf9ff996fab1e00ea6fd
andriy <evicandrij@gmail.com>
2024-01-30 21:05:24 +0200

    init

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/VocabBuilder">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
